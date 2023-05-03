c2260df89f54724d1d0564024d8302d48245cdaf
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-05-03 08:40:35 +0700

    Update exif.js

--- js/lib/exif.js
+++ js/lib/exif.js
@@ -78,7 +78,7 @@ fs.writeFileSync(tmpFileIn, wMedia)
 
 if (metadata.packname || metadata.author) {
 const img = new webp.Image()
-const json = { "sticker-pack-id": `https://github.com/KurrXd`, "sticker-pack-name": metadata.packname, "sticker-pack-publisher": metadata.author, "emojis": metadata.categories ? metadata.categories : [""] }
+const json = { "sticker-pack-id": `https://instagram.com/anggatzi123`, "sticker-pack-name": metadata.packname, "sticker-pack-publisher": metadata.author, "emojis": metadata.categories ? metadata.categories : [""] }
 const exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00])
 const jsonBuff = Buffer.from(JSON.stringify(json), "utf-8")
 const exif = Buffer.concat([exifAttr, jsonBuff])
@@ -99,7 +99,7 @@ fs.writeFileSync(tmpFileIn, wMedia)
 
 if (metadata.packname || metadata.author) {
 const img = new webp.Image()
-const json = { "sticker-pack-id": `https://github.com/KurrXd`, "sticker-pack-name": metadata.packname, "sticker-pack-publisher": metadata.author, "emojis": metadata.categories ? metadata.categories : [""] }
+const json = { "sticker-pack-id": `https://instagram.com/anggatzi123`, "sticker-pack-name": metadata.packname, "sticker-pack-publisher": metadata.author, "emojis": metadata.categories ? metadata.categories : [""] }
 const exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00])
 const jsonBuff = Buffer.from(JSON.stringify(json), "utf-8")
 const exif = Buffer.concat([exifAttr, jsonBuff])
@@ -120,7 +120,7 @@ fs.writeFileSync(tmpFileIn, wMedia)
 
 if (metadata.packname || metadata.author) {
 const img = new webp.Image()
-const json = { "sticker-pack-id": `https://github.com/KurrXd`, "sticker-pack-name": metadata.packname, "sticker-pack-publisher": metadata.author, "emojis": metadata.categories ? metadata.categories : [""] }
+const json = { "sticker-pack-id": `https://instagram.com/anggatzi123`, "sticker-pack-name": metadata.packname, "sticker-pack-publisher": metadata.author, "emojis": metadata.categories ? metadata.categories : [""] }
 const exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00])
 const jsonBuff = Buffer.from(JSON.stringify(json), "utf-8")
 const exif = Buffer.concat([exifAttr, jsonBuff])
